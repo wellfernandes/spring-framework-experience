50c514ca9d4aa16a1b138d3018a56b461604a5de
Welliton Fernandes Leal <wellfernandes@me.com>
2022-08-03 14:59:21 -0300

    dev class implemented

--- desafio-poo-dio/src/wellfernandes/com/github/dominio/Dev.java
+++ desafio-poo-dio/src/wellfernandes/com/github/dominio/Dev.java
@@ -2,6 +2,7 @@ package wellfernandes.com.github.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -11,15 +12,22 @@ public class Dev {
 	private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
 	public void inscreverBootCamp(Bootcamp bootcamp) {
-
+		this.conteudosInscritos.addAll(bootcamp.getConteudos());
+		bootcamp.getDevsInscritos().add(this);
 	}
 
 	public void progredir() {
-
+		Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+		if (conteudo.isPresent()) {
+			this.conteudosConcluidos.add(conteudo.get());
+			this.conteudosInscritos.remove(conteudo.get());
+		} else {
+			System.err.println("Você não está inscrito em nenhum conteúdo.");
+		}
 	}
 
-	public void calcularTotalXp() {
-
+	public double calcularTotalXp() {
+		return this.conteudosConcluidos.stream().mapToDouble(conteudo -> conteudo.calcularXp()).sum();
 	}
 
 	public String getNome() {
