1a50320bf3cf56ba65546257ed9d2214ca9a04ff
Welliton Fernandes Leal <wellfernandes@me.com>
2022-08-10 19:02:32 -0300

    avaliação física controller added

---
+++ academia-digital/src/main/java/one/digitalinnovation/academia/controllers/AvaliacaoFisicaController.java
@@ -0,0 +1,33 @@
+package one.digitalinnovation.academia.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import me.dio.academia.digital.entity.form.AvaliacaoFisicaForm;
+import me.dio.academia.digital.service.impl.AvaliacaoFisicaServiceImpl;
+import one.digitalinnovation.academia.entities.AvaliacaoFisica;
+
+@RestController
+@RequestMapping("/avaliacoes")
+public class AvaliacaoFisicaController {
+
+	@Autowired
+	private AvaliacaoFisicaServiceImpl service;
+
+	@PostMapping
+	public AvaliacaoFisica create(@RequestBody AvaliacaoFisicaForm form) {
+		return service.create(form);
+	}
+
+	@GetMapping
+	public List<AvaliacaoFisica> getAll() {
+		return service.getAll();
+	}
+
+}
