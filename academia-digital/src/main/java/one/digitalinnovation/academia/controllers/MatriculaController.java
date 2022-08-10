04a5faba64ec12c22b7f0941107bd293ba3ba49c
Welliton Fernandes Leal <wellfernandes@me.com>
2022-08-10 19:03:14 -0300

    matrícula controller  added

---
+++ academia-digital/src/main/java/one/digitalinnovation/academia/controllers/MatriculaController.java
@@ -0,0 +1,36 @@
+package one.digitalinnovation.academia.controllers;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import me.dio.academia.digital.entity.form.MatriculaForm;
+import me.dio.academia.digital.service.impl.MatriculaServiceImpl;
+import one.digitalinnovation.academia.entities.Matricula;
+
+@RestController
+@RequestMapping("/matriculas")
+public class MatriculaController {
+
+	@Autowired
+	private MatriculaServiceImpl service;
+
+	@PostMapping
+	public Matricula create(@Valid @RequestBody MatriculaForm form) {
+		return service.create(form);
+	}
+
+	@GetMapping
+	public List<Matricula> getAll(@RequestParam(value = "bairro", required = false) String bairro) {
+		return service.getAll(bairro);
+	}
+
+}
